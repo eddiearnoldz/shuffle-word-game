f6ef0639ffd32ce9517b8054367cf40effed3ab3
eddie arnold <eddiearnoldz@mac.com>
2024-10-08 16:15:06 -0700

    added ignore eslint on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // This will disable ESLint during the build
+  },
+};
 
 export default nextConfig;
